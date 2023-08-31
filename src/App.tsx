9ca1f65cb4bd1637fc1ceb56e01cf19e4461df2e
rockerzzzzz <120030813+hemanthkumarv24@users.noreply.github.com>
2023-08-31 21:18:47 +0530

    Trying to learn

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import ParticlesBackground from './components/ParticlesBackground';
+import Button from './components/Practice';
 
-
+function MyButton(){
+  return(
+    <div>
+    <button>CLICK ME</button>
+    console.log("Button Function loaded")
+    </div>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <ParticlesBackground />
       <Navbar/>
+      <MyButton/>
+      <Button/>
     </div>
   );
 }
---
+++ src/components/Practice.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+const Button:React.FC = () =>{
+    return(
+        <button>ClickMe</button>
+    )
+}
+
+export default Button;
