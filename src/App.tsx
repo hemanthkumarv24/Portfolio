1202211e614c89405f3a09ad95dfd0bacc94abc4
rockerzzzzz <120030813+hemanthkumarv24@users.noreply.github.com>
2023-09-04 17:45:58 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import { GlobalStyle } from './styles/global'
+// import { GlobalStyle } from './styles/global'
 import { Main } from './components/Main/main';
 // import { Header } from './components/Header/Header'
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyle></GlobalStyle>
+      {/* <GlobalStyle></GlobalStyle> */}
       <Navbar/>
       {/* <Header></Header> */}
       <Main></Main>
